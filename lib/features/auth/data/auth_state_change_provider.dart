8236d57f6b61d26995d35a2cbad608b7c812d099
mofakkharul-29 <mofakkharulislam29.info@gmail.com>
2026-02-17 01:36:13 +0600

    hellow

--- lib/features/auth/data/auth_state_change_provider.dart
+++ lib/features/auth/data/auth_state_change_provider.dart
@@ -1,8 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:to_do/features/auth/domain/app_user.dart';
 
-final authStateChangeProvider = StreamProvider<User?>((
-  ref,
-) {
-  return FirebaseAuth.instance.authStateChanges();
+final appUserProvider = StreamProvider<AppUser?>((ref) {
+  return FirebaseAuth.instance.authStateChanges().asyncMap((
+    User? firebaseUser,
+  ) async {
+    if (firebaseUser == null) {
+      return null;
+    }
+
+    final doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(firebaseUser.uid)
+        .get();
+
+    if (doc.exists) {
+      return AppUser.fromMap(doc.data()!);
+    } else {
+      return AppUser(
+        uid: firebaseUser.uid,
+        email: firebaseUser.email ?? '',
+        name: firebaseUser.displayName ?? 'Unknown',
+        photoUrl: firebaseUser.photoURL,
+        createdAt: DateTime.now(),
+      );
+    }
+  });
 });
