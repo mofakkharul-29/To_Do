c76d8173d19ca0f46ea65f2391cabaac376e1a47
mofakkharul-29 <mofakkharulislam29.info@gmail.com>
2026-02-23 02:18:57 +0600

    ui of the textformfield is completed

---
+++ lib/core/utils/custom_text_form_field.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final TextEditingController controller;
+  final FocusNode? focusNode;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final String labelText;
+  final IconData prefixIcon;
+  const CustomTextFormField({
+    super.key,
+    required this.controller,
+    required this.obscureText,
+    required this.labelText,
+    required this.prefixIcon,
+    this.focusNode,
+    this.keyboardType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      focusNode: focusNode,
+      keyboardType: keyboardType,
+      showCursor: true,
+      cursorErrorColor: Colors.red,
+      cursorRadius: Radius.circular(4),
+      cursorWidth: 3.0,
+      cursorColor: Colors.black87,
+      textInputAction: TextInputAction.done,
+      obscureText: obscureText,
+      style: TextStyle(
+        color: Colors.black87,
+        fontSize: 17,
+        fontWeight: FontWeight.w700,
+        letterSpacing: 1.1,
+      ),
+      onTapOutside: (event) {
+        focusNode!.unfocus();
+      },
+      onChanged: (value) {},
+      validator: (value) {},
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: TextStyle(
+          color: Colors.black87,
+          fontWeight: FontWeight.w800,
+        ),
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 8.5,
+          vertical: 18.5,
+        ),
+        prefixIcon: Icon(prefixIcon, size: 29.5),
+        prefixIconColor: Colors.black87,
+        filled: true,
+        fillColor: Colors.grey,
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black87,
+            width: 2.0,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(
+            color: Colors.black87,
+            width: 1.5,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/auth/presentation/login_screen.dart
+++ lib/features/auth/presentation/login_screen.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:to_do/core/utils/custom_text_form_field.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController =
+      TextEditingController();
+  final TextEditingController _passwordController =
+      TextEditingController();
+
+  final FocusNode _emailFocus = FocusNode();
+  final FocusNode _passwordFocus = FocusNode();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _emailFocus.dispose();
+    _passwordFocus.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Login page')),
+      backgroundColor: const Color.fromARGB(
+        255,
+        98,
+        96,
+        107,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8.0,
+            horizontal: 5,
+          ),
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(10),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextFormField(
+                    controller: _emailController,
+                    focusNode: _emailFocus,
+                    keyboardType:
+                        TextInputType.emailAddress,
+                    obscureText: false,
+                    labelText: 'Email',
+                    prefixIcon: Icons.email_outlined,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
